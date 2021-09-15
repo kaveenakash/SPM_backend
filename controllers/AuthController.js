0ed59c0fb6cdd56e3296b7429e6b5134c06e32b3
kaveenakashya <kaveenakashya@gmail.com>
2021-09-15 08:12:36 +0530

    Login endpoint completed

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -74,6 +74,54 @@ const SignUp = async (req, res, next) => {
   };
 
 
+// Login function
+const Login = async (req, res, next) => {
+    const { email, password } = req.body;
+    let existingUser;
+    try {
+      existingUser = await User.findOne({ email: email });
+    } catch (err) {
+      const error = new HttpError(
+        "Logging in failed, Please try again later",
+        500
+      );
+      return next(error);
+    }
+    if (!existingUser) {
+      const error = new HttpError("Invalid credentials,could not log in", 401);
+      return next(error);
+    }
+    let isValidPassword = false;
+    try {
+      isValidPassword = await bcrypt.compare(password, existingUser.password);
+    } catch (err) {
+      const error = new HttpError(
+        "Could not log you in,Please check your credentials and try again",
+        500
+      );
+      return next(error);
+    }
+  
+    if (!isValidPassword) {
+      const error = new HttpError("Invalid credentials,could not log in", 401);
+      return next(error);
+    }
+    let token;
+    try {
+      token = await jwt.sign(
+        { userId: existingUser.id, email: existingUser.email },
+        "supersecret_dont_share",
+        { expiresIn: "1h" }
+      );
+    } catch (err) {
+      const error = new HttpError("Login in failed,Please try again", 500);
+      return next(error);
+    }
+    res.json({userId:existingUser.id,email:existingUser.email,token:token});
+  };
+
+
   module.exports = {
-      SignUp
+      SignUp,
+      Login
   }
--- routes/auth-routes.js
+++ routes/auth-routes.js
@@ -4,10 +4,10 @@ const { check } = require("express-validator");
 
 const checkAuth = require('../middleware/check-auth')
 const fileUpload = require('../middleware/file-upload')
-const {SignUp} = require('../controllers/AuthController')
+const {SignUp,Login} = require('../controllers/AuthController')
 
 
 router.post("/signup",SignUp)
-
+router.post("/login",Login)
 
 module.exports = router  
